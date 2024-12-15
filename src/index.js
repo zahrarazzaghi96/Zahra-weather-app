9096875672a7874504bb8e97d020dfa8448ffa39
zahrarazzaghi96 <zahra.razzaghi96@gmail.com>
2024-12-15 09:49:45 +0330

    update HTML and Javascript

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-function refreshWeatherData(response) {
-    console.log(response.data);
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
   }
   function searchCity(city) {
-    let apiKey = "46a7o7054af22ba1t49c85122b081838";
+    let apiKey = "483abfb4bt0oa2508177f7baa6732b73";
     let apiUrl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
-    axios.get(apiUrl).then(refreshWeatherData);
+    axios.get(apiUrl).then(refreshWeather);
   }
   function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityWeatherElement = document.querySelector("#city-weather");
-    cityWeatherElement.innerHTML = searchInput.value;
     searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  searchCity("Zarqan");
