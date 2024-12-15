5ea5e3d907d8a3836d4210e64a19d4b3277828f2
zahrarazzaghi96 <zahra.razzaghi96@gmail.com>
2024-12-15 08:56:20 +0330

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-function handleSearchSubmit(event){
+function refreshWeatherData(response) {
+    console.log(response.data);
+  }
+  function searchCity(city) {
+    let apiKey = "46a7o7054af22ba1t49c85122b081838";
+    let apiUrl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+    axios.get(apiUrl).then(refreshWeatherData);
+  }
+  function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-}
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+    let cityWeatherElement = document.querySelector("#city-weather");
+    cityWeatherElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
