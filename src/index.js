f888f78635917193c1bea69c200fd1b71cf4d6de
zahrarazzaghi96 <zahra.razzaghi96@gmail.com>
2024-12-15 15:20:12 +0330

    fix some problems

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function refreshWeather(response) {
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
+
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
@@ -46,37 +47,42 @@ function refreshWeather(response) {
     searchCity(searchInput.value);
   }
 
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[date.getDay()];
+  }
+
   function getForecast (city) {
-    let apiKey = "occ5995cba7146ce6c3fet3758a0a3ce";
-    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    let apiKey = "483abfb4bt0oa2508177f7baa6732b73";
+    let apiUrl = `"https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
     axios(apiUrl).then(displayForecast);
   } 
   function displayForecast(response) {
-    console.log(response.data)
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml = "";
-    days.forEach(function (day) {
-      forecastHtml =
-        forecastHtml +
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHtml =
+          forecastHtml +
         `
         <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-              <strong>15º</strong>
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
             </div>
-            <div class="weather-forecast-temperature">9º</div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º
           </div>
         </div>
       `;
-    });
+    }
+  });
     let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
   }
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
-  searchCity("Zarqan");
-
-  getForecast("Zarqan");
+  searchCity("Los Angeles");
